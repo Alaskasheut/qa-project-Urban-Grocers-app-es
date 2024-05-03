62911ef4481542baf43b75cb960b59e5924a3459
Alma Fiesco <47203929+Alaskasheut@users.noreply.github.com>
2024-05-02 19:40:40 -0600

    Update_project_configuration.py
    Paths to specific request

--- configuration.py
+++ configuration.py
@@ -1,3 +1,10 @@
-URL_SERVICE = # Inserta tu dirección de URL sin la barra diagonal al final
-CREATE_USER_PATH = # Almacena la ruta para crear un usuario o usuaria en esta variable
-KITS_PATH = # Almacena la ruta para crear un kit en esta variable
+URL_SERVICE='https://cnt-41c69673-6171-4d8a-aa99-de8266b6877c.containerhub.tripleten-services.com'
+#------------------------------POST-----------------------------
+DOCS_PATH='/docs/'
+LOG_MAIN_PATH='/api/logs/main/'
+USER_TABLE_PATH='/api/db/resources/user_model.csv'
+
+#------------------------------POST-----------------------------
+CREATE_USER_PATH='/api/v1/users/' #Create a new user
+PRODUCTS_KITS_PATH='/api/v1/products/kits/' #List of products
+CREATE_KIT_PATH="/api/v1/kits"#Create a new kit
